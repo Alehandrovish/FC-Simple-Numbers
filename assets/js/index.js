1225ea0092ff2e98d33c40f678a2a17cc8980121
Alex <sascha.rumin@gmail.com>
2025-01-20 16:32:39 +0200

    Optimaze calculate speed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 const breakPoint = 100;
 for (let i = 2; i <= breakPoint; i++) {
   let counter = 0;
-  for (let j = 1; j <= breakPoint; j++) {
+  for (let j = 1; j <= i; j++) {
     let result = i % j;
     if (result == 0) {
       counter++;
